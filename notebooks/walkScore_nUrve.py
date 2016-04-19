8d108699f644b98bfb3a34f0254bf614f5e9ba06
Sara Arango <sarangofc@gmail.com>
2016-04-19 16:17:38 -0400

    Making the calls for our data set.

--- notebooks/walkScore_nUrve.py
+++ notebooks/walkScore_nUrve.py
@@ -10,11 +10,13 @@ import urllib2
 import requests
 import json
 import xml.etree.ElementTree as ET
+import random
 
 master = pd.read_csv('nUrve_master_0321.csv')
 sampleB = pd.read_csv('pilotB_sample1.csv')
 
-mykey = 'b901157a817d3058367b22fdabcc6596'
+
+group_keys = ['b901157a817d3058367b22fdabcc6596'] # Sara
 
 
 master['uniqueLatLon'] = master.apply(lambda x: str(x.GPS_LAT)+'|'+str(x.GPS_LON), axis=1)
@@ -46,17 +48,32 @@ State = 'WA'
 Zip = '9810' # ZIPCODE
 #str_call = 'http://api.walkscore.com/score?format=xml&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat='+LAT+'&lon='+LON+'&wsapikey='+mykey+'&format=JSON'
 
-call = 'http://api.walkscore.com/score?format=xml&address='+Stnum+'%'+Stname+'%20'+st_nm_su+'%20'+City+'%20'+State+'%20'+Zip+'&lat='+LAT+'&'+'lon='+LON+'&wsapikey='+mykey+'&format=json'
-url_web = 'http://api.walkscore.com/score?format=xml&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085&lon=-122.3295&wsapikey='+mykey+'&format=XML'
-request = requests.get(url_web)
+nUrv_add = nUrv_add.rename(index=str, columns={"uniqueLatL": "uniqueLatLon"});
+data = pd.merge(nUrv_add,master,on='uniqueLatLon')
 
-tree = ET.fromstring(request.content)
-for child in tree.findall('{http://walkscore.com/2008/results}walkscore'):
-    print child.text #This is the walkscore
+for key in group_keys:
+    rows = random.sample(data.index, 5000)
+    samp = data.ix[rows]
+    data = data.drop[rows]
+    for i in range(0,1):
+        Stnum = samp['ST_NUM'].ix[rows[i]] #ST_NUM
+        Stname = samp['ST_NAME'].ix[rows[i]] 
+        st_nm_su = samp['ST_NAME_SU'].ix[rows[i]]
+        LAT = samp['Latitude'].ix[rows[i]]
+        LON = samp['Longitude'].ix[rows[i]] # sample -122.3295
+        City = 'Boston'
+        State = 'MA'
+        Zip = samp['ZIPCODE'].ix[rows[i]] # ZIPCODE
+        
+        call = 'http://api.walkscore.com/score?format=xml&address='+str(Stnum)+'%'+str(Stname)+'%20'+str(st_nm_su)+'%20'+str(City)+'%20'+str(State)+'%20'+str(Zip)+'&lat='+str(LAT)+'&'+'lon='+str(LON)+'&wsapikey='+str(key)+'&format=XML' 
+        #url_web = 'http://api.walkscore.com/score?format=xml&address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085&lon=-122.3295&wsapikey='+key+'&format=XML'
+        request = requests.get(call)
+        
+        tree = ET.fromstring(request.content)
+        for child in tree.findall('{http://walkscore.com/2008/results}walkscore'):
+            print child.text #This is the walkscore
 
-
-
-
+    
 
 #url = 'https://nycopendata.socrata.com/views/%s' % (dataId)
 
